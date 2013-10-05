9d1ec49aaf0858dc69b9f13f16b993012431ea40
Bryce Mata <bryce@brycemata.com>
2013-10-04 22:27:48 -0400

    Impoved versatility of the menu engine
    Added the options argument to separate the options from prompt.

--- lib/tutorial.rb
+++ lib/tutorial.rb
@@ -6,27 +6,40 @@ class Tutorial
 		@song = Gosu::Sample.new("#{$ASSET_PATH}/TutorialMenu.ogg")
 		@song.play(1,1,true)
 		name_menu = Menu.new
-		name_menu.add "Rumplestilskin"
-		name_menu.add "Beetlejuice"
-		name_menu.add "Bosie"
+		name_menu.add_prompt "Welcome to Tutorial: The Game. To continue, please select the number corresponding with the letter 'A'."
+		name_menu.add_options "A"
+		name_menu.add_options "B"
+		name_menu.add_options "C"
+		name_menu.prompt
+	
+		name_menu = Menu.new
+		name_menu.add_prompt "Great Job! Please select the number you chose to successfully pass the first level."
+		name_menu.add_options "2"
+		name_menu.add_options "1"
+		name_menu.add_options "3"
 		name_menu.prompt
 	end
 end
 
 class Menu
-	def initialize(prompt = [])
+	def initialize(options = [], prompt = [])
+		@options = options
 		@prompt = prompt
 		@sounds = File.join(File.dirname(__FILE__), '..', 'sounds')
 	end
-	def add(new_prompt)
+	def add_options(new_options)
+		@options.push new_options
+	end
+	def add_prompt(new_prompt)
 		@prompt.push new_prompt
 	end
 	def prompt
-		@prompt.each_with_index do |text,i|
+		puts @prompt
+		@options.each_with_index do |text,i|
 			puts "#{i+1}. #{text}"
 		end
 		choice = gets.chomp
-		if @prompt.fetch(choice.to_i - 1,nil) and choice.to_i != 0
+		if @options.fetch(choice.to_i - 1,nil) and choice.to_i != 0
 			return choice.to_i
 		else
 			puts "I'm sorry but \"#{choice}\" is an invalid choice."
