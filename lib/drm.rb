14548188acec007204738f8f4a489bd13ef50eaf
Kevin Phelps <kphelps@engineyard.com>
2013-10-18 23:16:07 -0400

    fixed url in drm.rb

--- lib/drm.rb
+++ lib/drm.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 class Drm
 	def check
 		begin
-			RestClient.get('http://tutorialthegame.com/xdrm')
+			RestClient.get('http://tutorialthegame.com/drm')
 			return 'drm connected'
 		rescue Exception => e
 			return 'error checking drm: ' + e.message
